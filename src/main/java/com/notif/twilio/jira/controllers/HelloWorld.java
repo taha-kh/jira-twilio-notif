9f419ff68caa783fda89cddbccbb4e433841fcf3
unknown <tah.khelifi@gmail.com>
2020-04-14 17:58:20 +0200

    Setup Thymeleaf with Atlassian Spring Boot Project

--- src/main/java/com/notif/twilio/jira/controllers/HelloWorld.java
+++ src/main/java/com/notif/twilio/jira/controllers/HelloWorld.java
@@ -2,16 +2,28 @@ package com.notif.twilio.jira.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.atlassian.connect.spring.AtlassianHostUser;
+import com.notif.twilio.jira.ui.models.Greeting;
+
 
 @Controller
 public class HelloWorld {
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String getHelloPage(@AuthenticationPrincipal AtlassianHostUser hostUser) {
-		return "hello";
+
+	
+	@GetMapping("/greeting")
+	  public String greetingForm(@AuthenticationPrincipal AtlassianHostUser hostUser, Model model) {
+	    model.addAttribute("greeting", new Greeting());
+	    return "hello";
+	  }
+
+	  @PostMapping("/greeting")
+	  public String greetingSubmit(@ModelAttribute Greeting greeting) {
+		  Greeting g = greeting;
+		return "form";
 	}
 }
